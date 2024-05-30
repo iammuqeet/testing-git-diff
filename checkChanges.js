adb53d899e4e5b691581e48465529dc2b2fc609c
Muqeet Ahmad <iammuqeetahmad@gmail.com>
2024-05-30 11:18:14 -0700

    Initial commit in new branch

---
+++ checkChanges.js
@@ -0,0 +1,25 @@
+const simpleGit = require('simple-git');
+const path = require('path');
+
+const git = simpleGit();
+
+async function checkSidenavChanges() {
+	try {
+		// Get the list of changed files
+		const statusSummary = await git.status();
+		const changedFiles = statusSummary.files.map((file) => file.path);
+
+		// Check if any of the changed files are in the sidenav folder
+		const sidenavPath = path.join('sidenav');
+		const hasSidenavChanges = changedFiles.some((file) => file.startsWith(sidenavPath));
+
+		return hasSidenavChanges;
+	} catch (error) {
+		console.error('Error checking sidenav changes:', error);
+		return false;
+	}
+}
+
+checkSidenavChanges().then((hasSidenavChanges) => {
+	console.log('Has sidenav changes:', hasSidenavChanges);
+});
---
+++ check_changes.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Run the Node.js script and capture its output
+OUTPUT=$(node checkChanges.js)
+
+# Print the output to the terminal
+echo "$OUTPUT"
